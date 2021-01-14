dd02f13fbf91df29e79c84df1d9e1e27c04434c3
Kai Xu <kevxu@my.yorku.ca>
2021-01-14 17:52:37 -0500

    test

--- src/rules/sampleRule.test.js
+++ src/rules/sampleRule.test.js
@@ -1,6 +1,6 @@
 const sampleRule = require("./sampleRule.js");
 describe(sampleRule, () => {
-  it("should pass return null as error!", () => {
+  it("should pass return null as error! Please check.", () => {
     expect.hasAssertions();
     const context = {
       request: {
