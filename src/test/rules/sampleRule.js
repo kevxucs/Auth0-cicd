a165bd8503cc2eba71eac053e27850a902122570
Kai Xu <kevxu@my.yorku.ca>
2021-01-18 19:01:54 +0000

    Add changes

--- src/test/rules/sampleRule.js
+++ src/test/rules/sampleRule.js
@@ -1,6 +1,6 @@
 (() => {
   function rule(user, context, callback) {
-    // test on dev
+    // test on dev 2
     console.log("IMPORT");
     console.log("Context -> ", context);
     console.log(context.request.query.scope);
