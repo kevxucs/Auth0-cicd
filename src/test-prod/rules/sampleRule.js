4917d0fbb095af586c80c02d83085fe462b133f0
Kai Xu <kevxu@my.yorku.ca>
2021-01-18 19:48:51 +0000

    Add changes

---
+++ src/test-prod/rules/sampleRule.js
@@ -0,0 +1,14 @@
+(() => {
+  function rule(user, context, callback) {
+    // test on prod pull
+    console.log("IMPORT");
+    console.log("Context -> ", context);
+    console.log(context.request.query.scope);
+    return callback(null, user, context);
+  }
+  if (module) {
+    module.exports = rule;
+  } else {
+    return rule;
+  }
+})() // prettier-ignore
