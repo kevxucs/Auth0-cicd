3d3be02ef4a750ce3e808b2074880419fe5c8559
Kai Xu <kevxu@my.yorku.ca>
2021-01-18 14:47:38 -0500

    test push prod

--- src/prod/sampleRule.js
+++ src/prod/sampleRule.js
@@ -1,6 +1,6 @@
 (() => {
   function rule(user, context, callback) {
-    // test on prod pull
+    // test on prod pull!
     console.log("IMPORT");
     console.log("Context -> ", context);
     console.log(context.request.query.scope);
