8d80d2cce21d42c4d4950ca0b9664c9bcda5f3be
Kai Xu <kevxu@my.yorku.ca>
2021-01-19 09:28:13 -0500

    test v5

--- src/dev/sampleRule.js
+++ src/dev/sampleRule.js
@@ -1,6 +1,6 @@
 (() => {
   function rule(user, context, callback) {
-    // test on prod pull v5
+    // test on prod pull v5!
     console.log("IMPORT");
     console.log("Context -> ", context);
     console.log(context.request.query.scope);
